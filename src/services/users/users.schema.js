a4c8bedf3fa869efa12f1abe4408dfd2be36f5f8
Greymouth <thejadenjack@gmail.com>
2017-08-19 23:48:19 -0600

    user schema

--- src/services/users/users.schema.js
+++ src/services/users/users.schema.js
@@ -5,5 +5,14 @@ module.exports = new Schema({
 	user: { type: String, required: true },
 	password: { type: String, required: true },
 	email: String,
-	name: String
+	name: String,
+	stats: {
+		kills: { type: Number, default: 0, range: ['>=', 0, 'cannot have less than 0 kills.'] },
+		deaths: { type: Number, default: 0, range: ['>=', 0, 'cannot have less than 0 deaths.'] },
+		bombs_placed: { type: Number, default: 0, range: ['>=', 0, 'cannot have less than 0 bombs placed.'] },
+		powerups_used: { type: Number, default: 0, range: ['>=', 0, 'cannot have less than 0 powerups used.'] },
+		games_played: { type: Number, default: 0, range: ['>=', 0, 'cannot have less than 0 games played.'] },
+		wins: { type: Number, default: 0, range: ['>=', 0, 'cannot have less than 0 wins.'] },
+		losses: { type: Number, default: 0, range: ['>=', 0, 'cannot have less than 0 losses.'] }
+	}
 });
