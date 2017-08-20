e46f369d343084e65905e858b9cca1cb804fd7d4
Greymouth <thejadenjack@gmail.com>
2017-08-19 19:21:20 -0600

    Fixing authentication usernameField

--- src/services/users/users.schema.js
+++ src/services/users/users.schema.js
@@ -2,7 +2,7 @@ const feathers_schema = require('feathers-schema');
 const Schema = feathers_schema.Schema;
 
 module.exports = new Schema({
-	username: { type: String, required: true },
+	user: { type: String, required: true },
 	password: { type: String, required: true },
 	email: String,
 	name: String
