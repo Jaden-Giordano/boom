b8ce29feb735955ad0bbaf283492f0f8c9f4b157
Greymouth <thejadenjack@gmail.com>
2017-08-25 13:16:05 -0600

    Login Auth and redirection

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,72 @@ angular.module('boom', ['ngRoute']) // eslint-disable-line no-undef
 	.config(function($routeProvider) {
 		$routeProvider
 			.when('/', {
-				templateUrl: 'view/home.html'
+				templateUrl: 'view/home.html',
+				controller: 'home',
+				resolve: 'auth'
 			})
 			.when('/login', {
 				templateUrl: 'view/login.html',
-				controller: 'login'
+				controller: 'login',
+				resolve: 'auth'
+			})
+			.when('/users/:id', {
+				templateUrl: 'view/user.html',
+				controller: 'user'
 			});
 	})
-	.controller('login', function() {
-	});
+	.factory('auth', function($http) {
+		return {
+			accept: function() {
+				if (!localStorage.accessToken)
+					return false;
+
+				return $http({
+					method: 'POST',
+					url: 'http://localhost:3030/authentication',
+					headers: {
+						"authorization": "Bearer " + localStorage.accessToken
+					}
+				}).then(function(res) {
+					if (!res.data.accessToken)
+						return false;
+
+					return true;
+				}, function(err) {
+					console.log(err);
+
+					return false;
+				});
+			}
+		}
+	})
+	.controller('home', ['$scope', '$location', 'auth', function($scope, $location, auth) {
+		if (!auth.accept())
+			$location.path('/login');
+	}])
+	.controller('login', ['$scope', '$http', '$location', 'auth', function($scope, $http, $location, auth) {
+		if (auth.accept())
+			$location.path('/');
+
+		$scope.login = function(user) {
+			$http({
+				method: 'POST',
+				url: 'http://localhost:3030/authentication',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				data: {
+					strategy: 'local',
+					user: user.user,
+					password: user.password
+				}
+			}).then(function(res) {
+				if (res.data && res.data.accessToken) {
+					localStorage.accessToken = res.data.accessToken
+					$location.path('/');
+				}
+			}, function(err) {
+				console.log('Unauthorized');
+			});
+		}
+	}]);
