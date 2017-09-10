8f9f2729cad89f746458210ba4b229d4295f1cf9
Greymouth <thejadenjack@gmail.com>
2017-09-10 15:44:05 -0600

    Refactoring and promisifying stuffs

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 angular.module('boom', ['ngRoute']) // eslint-disable-line no-undef
-	.config(function($routeProvider) {
+	.config($routeProvider => {
 		$routeProvider
 			.when('/', {
 				templateUrl: 'view/home.html',
@@ -18,127 +18,112 @@ angular.module('boom', ['ngRoute']) // eslint-disable-line no-undef
 				controller: 'UserController'
 			});
 	})
-	.factory('auth', function($http, $location) {
-		return new Promise((resolve, reject) => {
-			if (!localStorage.accessToken)
-				return reject('Not logged in.');
-
-			return $http({
-				method: 'POST',
-				url: 'http://localhost:3030/authentication',
-				headers: {
-					'authorization': 'Bearer ' + localStorage.accessToken
-				}
-			}).then(function(res) {
-				if (!res.data.accessToken) {
-					return reject('Invalid session.');
-				}
-
-				return resolve(res.data.accessToken);
-			}, function(err) {
-				console.log(err); // eslint-disable-line no-console
-
-				return reject(err);
-			});
-		});
-	})
-	.factory('nav', function($rootScope) {
+	.factory('util', ['$http', '$rootScope', ($http, $rootScope) => {
 		return {
-			updateProfile: function() {
+			verify: () => {
+				return new Promise((resolve, reject) => {
+					if (!localStorage.accessToken)
+						return reject('Not logged in.');
+
+					return $http({
+						method: 'POST',
+						url: 'http://localhost:3030/utilentication',
+						headers: {
+							'utilorization': 'Bearer ' + localStorage.accessToken
+						}
+					}).then(res => {
+						if (!res.data.accessToken) {
+							delete localStorage.accessToken;
+
+							return reject('Invalid session.');
+						}
+
+						localStorage.accessToken = res.data.accessToken;
+
+						return resolve(res.data.accessToken);
+					}, err => {
+						delete localStorage.accessToken;
+
+						return reject(err);
+					});
+				});
+			},
+			updateProfile: () => {
 				if (localStorage.accessToken) {
-					let data = localStorage.accessToken.split('.')[1];
-					if (data) {
-						let obj = JSON.parse(atob(data));
-						if (obj) {
+					if (let data = localStorage.accessToken.split('.')[1]) {
+						if (let obj = JSON.parse(atob(data))) {
 							$rootScope.user_id = obj.userId;
 						}
 					}
 				}
 			}
 		};
-	})
-	.controller('HomeController', ['$scope', '$location', 'nav', 'auth', function($scope, $location, nav, auth) {
-		auth.then(token => localStorage.accessToken = token, err => $location.path('/login'));
-
-		nav.updateProfile();
 	}])
-	.controller('LoginController', ['$scope', '$http', '$location', 'auth', function($scope, $http, $location, auth) {
-		auth.then(token => $location.path('/'));
-
-		$scope.login = function(user) {
-			$http({
-				method: 'POST',
-				url: 'http://localhost:3030/authentication',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				data: {
-					strategy: 'local',
-					user: user.user,
-					password: user.password
-				}
-			}).then(function(res) {
-				if (res.data && res.data.accessToken) {
-					localStorage.accessToken = res.data.accessToken;
-					$location.path('/');
-				}
-			}, function(err) {
-				console.log('Unauthorized: ' + err); // eslint-disable-line no-console
-			});
-		};
+	.controller('HomeController', ['$scope', '$location', 'nav', 'util', ($scope, $location, nav, util) => {
+		return util.verify().then(() => {
+			return nav.updateProfile();
+		}, () => $location.path('/login'));
 	}])
-	.controller('SignupController', ['$scope', '$http', '$location', 'auth', function($scope, $http, $location, auth) {
-		auth.then(token => $location.path('/'));
-
-		$scope.signup = function(user) {
-			if (user) {
-				if (user.password !== user.password_again)
-					$scope.password_unmatch = true;
-				else
-					$scope.password_unmatch = false;
-				if (!user.password)
-					$scope.password_required = true;
-				else
-					$scope.password_required = false;
-				if (!user.password_again)
-					$scope.password_again_required = true;
-				else
-					$scope.password_again_required = false;
-				if (!user.user)
-					$scope.user_required = true;
-				else
-					$scope.user_required = false;
-				if (!user.email)
-					$scope.email_required = true;
-				else
-					$scope.email_required = false;
-
+	.controller('LoginController', ['$scope', '$http', '$location', 'util', ($scope, $http, $location, util) => {
+		util.verify().then(() => $location.path('/'), () => {
+			$scope.login = user => {
 				$http({
-					method: 'post',
-					url: 'http://localhost:3030/users',
+					method: 'POST',
+					url: 'http://localhost:3030/utilentication',
 					headers: {
 						'Content-Type': 'application/json'
 					},
 					data: {
+						strategy: 'local',
 						user: user.user,
-						email: user.email,
 						password: user.password
 					}
-				}).then(function(res) { // eslint-disable-line
-
-				}, function(err) { // eslint-disable-line
-
+				}).then(res => {
+					if (res.data && res.data.accessToken) {
+						localStorage.accessToken = res.data.accessToken;
+						$location.path('/');
+					}
+				}, err => {
+					console.log('Unutilorized'); // eslint-disable-line no-console
 				});
-			} else {
-				$scope.missing_all_fields = true;
-			}
-		};
+			};
+		});
 	}])
-	.controller('UserController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-		$http.get('http://localhost:3030/users/' + $routeParams.id).then(function(res) {
-			if (res.data) {
-				let user = res.data;
-				if (!res.data.stats) {
+	.controller('SignupController', ['$scope', '$http', '$location', 'util', ($scope, $http, $location, util) => {
+		util.verify().then(token => $location.path('/'), () => {
+			$scope.signup = (user) => {
+				if (user) {
+					$scope.password_unmatch = user.password !== user.password_again;
+					$scope.password_required = !user.password;
+					$scope.password_again_required = !user.password_again;
+					$scope.user_required = !user.user;
+					$scope.email_required = !user.email;
+
+					$http({
+						method: 'post',
+						url: 'http://localhost:3030/users',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						data: {
+							user: user.user,
+							email: user.email,
+							password: user.password
+						}
+					}).then(() => {}, err => { // eslint-disable-line
+
+					});
+				} else {
+					$scope.missing_all_fields = true;
+				}
+			};
+		});
+	}])
+	.controller('UserController', ['$scope', '$http', '$routeParams', ($scope, $http, $routeParams) => {
+		$http.get('http://localhost:3030/users/' + $routeParams.id).then(res => {
+			let user = res.data;
+			if (user) {
+				if (!user.stats) {
 					user.stats = {
 						kills: 0,
 						deaths: 0,
@@ -151,7 +136,5 @@ angular.module('boom', ['ngRoute']) // eslint-disable-line no-undef
 				}
 				$scope.user = user;
 			}
-		}, function(err) {
-			console.log(err); // eslint-disable-line no-console
-		});
+		}, err => console.log(err)); // eslint-disable-line no-console);
 	}]);
